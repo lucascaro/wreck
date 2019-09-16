c4af2baeccef1c3d14e1883fc9e7667a1c4ea32c
Lucas Caro <lucascaro@gmail.com>
2019-09-16 08:56:04 -0700

    formatting'

--- src/helpers/PersistentState.ts
+++ src/helpers/PersistentState.ts
@@ -17,13 +17,19 @@
  *
  */
 
-import { createWriteStream, WriteStream, createReadStream, renameSync, existsSync } from 'fs';
-import { WorkPayload, ResultPayload } from './Message';
-import { Writable } from 'stream';
-import * as split2 from 'split2';
-import * as Debug from 'debug';
+import {
+  createWriteStream,
+  WriteStream,
+  createReadStream,
+  renameSync,
+  existsSync
+} from "fs";
+import { WorkPayload, ResultPayload } from "./Message";
+import { Writable } from "stream";
+import * as split2 from "split2";
+import * as Debug from "debug";
 
-const debug = Debug('wreck:helpers:persistence');
+const debug = Debug("wreck:helpers:persistence");
 
 let fileName: string;
 
@@ -43,58 +49,72 @@ export namespace PersistentState {
   export function readState(
     outAllURLs: Set<String>,
     outWorkQueue: Map<string, WorkPayload>,
-    options?: Partial<{retryFailed: boolean}>,
-    ): Promise<ResultPayload[]> {
+    options?: Partial<{ retryFailed: boolean }>
+  ): Promise<ResultPayload[]> {
     return new Promise((resolve, reject) => {
-      if (!init()) { throw new Error('persistent state not initialized'); }
+      if (!init()) {
+        throw new Error("persistent state not initialized");
+      }
       const allWork: ResultPayload[] = [];
       const retryFailed = options && options.retryFailed;
       if (!existsSync(fileName)) {
         resolve(allWork);
         return;
       }
-      
-      createReadStream(fileName, { encoding: 'utf-8' })
-      .pipe(split2())
-      .pipe(new Writable({
-        write(chunk, encoding, callback) {
-          const result: ResultPayload = JSON.parse(chunk);
-          // debug({ ...result, neighbours: result.neighbours.length });
-          outAllURLs.add(result.url);
-          allWork.push(result);
-          result.neighbours.forEach((neighbour) => {
-            // Ignore URLs that were already added.
-            if (!outAllURLs.has(neighbour)) {
-              const payload: WorkPayload = {
-                url: neighbour,
-                referrer: result.url,
-                depth: result.depth + 1,
-              };
-              outWorkQueue.set(neighbour, payload);
-              outAllURLs.add(neighbour);
+
+      createReadStream(fileName, { encoding: "utf-8" })
+        .pipe(split2())
+        .pipe(
+          new Writable({
+            write(chunk, encoding, callback) {
+              const result: ResultPayload = JSON.parse(chunk);
+              // debug({ ...result, neighbours: result.neighbours.length });
+              outAllURLs.add(result.url);
+              allWork.push(result);
+              result.neighbours.forEach(neighbour => {
+                // Ignore URLs that were already added.
+                if (!outAllURLs.has(neighbour)) {
+                  const payload: WorkPayload = {
+                    url: neighbour,
+                    referrer: result.url,
+                    depth: result.depth + 1
+                  };
+                  outWorkQueue.set(neighbour, payload);
+                  outAllURLs.add(neighbour);
+                }
+              });
+              if (
+                !retryFailed ||
+                (result.statusCode >= 200 && result.statusCode < 400)
+              ) {
+                outWorkQueue.delete(result.url);
+              }
+              callback();
             }
-          });
-          if (!retryFailed || result.statusCode >= 200 && result.statusCode < 400) {
-            outWorkQueue.delete(result.url);
-          }
-          callback();
-        },
-      }))
-      .on('finish', () => resolve(allWork))
-      .on('error', reject);
+          })
+        )
+        .on("finish", () => resolve(allWork))
+        .on("error", reject);
     });
   }
 
   export function write(chunk: any) {
-    if (!init()) { throw new Error('persistent state not initialized'); }
+    if (!init()) {
+      throw new Error("persistent state not initialized");
+    }
     if (!outStream) {
-      outStream = createWriteStream(fileName, { encoding: 'utf-8', flags: 'a' });
+      outStream = createWriteStream(fileName, {
+        encoding: "utf-8",
+        flags: "a"
+      });
     }
     outStream.write(chunk);
   }
 
   export function resetState() {
-    if (!init()) { throw new Error('persistent state not initialized'); }
+    if (!init()) {
+      throw new Error("persistent state not initialized");
+    }
     if (outStream) {
       outStream.close();
       outStream = null;
